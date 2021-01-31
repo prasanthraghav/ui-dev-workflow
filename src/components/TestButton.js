8982dd2f1cc489391407bd844009483bb5cff665
Prasanth Raghavan <prasanthr@qburst.com>
2021-01-31 22:01:34 +0530

    Added TestButton

---
+++ src/components/TestButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+function TestButton() {
+    return (
+        <button className="test-button button-text">Test Button</button>
+    )
+}
+
+export default TestButton
---
+++ src/components/TestButton.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import TestButton from './TestButton';
+
+export default {
+  title: 'Example/TestButton',
+  component: TestButton
+}
+
+const Template = () => <TestButton />;
+
+export const Primary = Template.bind({});
